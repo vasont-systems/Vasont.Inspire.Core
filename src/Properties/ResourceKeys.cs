c7896b0af33803ce93c3dbfe0660b2e8e4e29364
Rob Kennedy (Admin) <rkennedy@vasont.com>
2020-01-23 12:19:51 -0500

    missed running of resource keys tool.

--- src/Properties/ResourceKeys.cs
+++ src/Properties/ResourceKeys.cs
@@ -12,16 +12,6 @@ namespace Vasont.Inspire.Core.Properties
     public static class ResourceKeys
     {
         /// <summary>
-        /// Gets the key for the ImageDefaultIcon resource string.
-        /// </summary>
-        public const string ImageDefaultIcon = "ImageDefaultIcon"; 
-
-        /// <summary>
-        /// Gets the key for the VideoDefaultIcon resource string.
-        /// </summary>
-        public const string VideoDefaultIcon = "VideoDefaultIcon"; 
-
-        /// <summary>
         /// Gets the key for the ArgumentMustBeGreaterThanZeroErrorFormat resource string.
         /// </summary>
         public const string ArgumentMustBeGreaterThanZeroErrorFormat = "ArgumentMustBeGreaterThanZeroErrorFormat"; 
@@ -32,8 +22,128 @@ namespace Vasont.Inspire.Core.Properties
         public const string DeserializeFileNotFoundErrorText = "DeserializeFileNotFoundErrorText"; 
 
         /// <summary>
+        /// Gets the key for the ImageDefaultIcon resource string.
+        /// </summary>
+        public const string ImageDefaultIcon = "ImageDefaultIcon"; 
+
+        /// <summary>
         /// Gets the key for the InvalidFileArgumentErrorText resource string.
         /// </summary>
         public const string InvalidFileArgumentErrorText = "InvalidFileArgumentErrorText"; 
+
+        /// <summary>
+        /// Gets the key for the LabelAndText resource string.
+        /// </summary>
+        public const string LabelAndText = "LabelAndText"; 
+
+        /// <summary>
+        /// Gets the key for the LabelApplicationText resource string.
+        /// </summary>
+        public const string LabelApplicationText = "LabelApplicationText"; 
+
+        /// <summary>
+        /// Gets the key for the LabelContainsText resource string.
+        /// </summary>
+        public const string LabelContainsText = "LabelContainsText"; 
+
+        /// <summary>
+        /// Gets the key for the LabelDebugText resource string.
+        /// </summary>
+        public const string LabelDebugText = "LabelDebugText"; 
+
+        /// <summary>
+        /// Gets the key for the LabelDoesNotContainText resource string.
+        /// </summary>
+        public const string LabelDoesNotContainText = "LabelDoesNotContainText"; 
+
+        /// <summary>
+        /// Gets the key for the LabelDoesNotEndWithText resource string.
+        /// </summary>
+        public const string LabelDoesNotEndWithText = "LabelDoesNotEndWithText"; 
+
+        /// <summary>
+        /// Gets the key for the LabelDoesNotStartWithText resource string.
+        /// </summary>
+        public const string LabelDoesNotStartWithText = "LabelDoesNotStartWithText"; 
+
+        /// <summary>
+        /// Gets the key for the LabelEndsWithText resource string.
+        /// </summary>
+        public const string LabelEndsWithText = "LabelEndsWithText"; 
+
+        /// <summary>
+        /// Gets the key for the LabelErrorText resource string.
+        /// </summary>
+        public const string LabelErrorText = "LabelErrorText"; 
+
+        /// <summary>
+        /// Gets the key for the LabelFatalText resource string.
+        /// </summary>
+        public const string LabelFatalText = "LabelFatalText"; 
+
+        /// <summary>
+        /// Gets the key for the LabelGeneralText resource string.
+        /// </summary>
+        public const string LabelGeneralText = "LabelGeneralText"; 
+
+        /// <summary>
+        /// Gets the key for the LabelInformationText resource string.
+        /// </summary>
+        public const string LabelInformationText = "LabelInformationText"; 
+
+        /// <summary>
+        /// Gets the key for the LabelNoText resource string.
+        /// </summary>
+        public const string LabelNoText = "LabelNoText"; 
+
+        /// <summary>
+        /// Gets the key for the LabelOrText resource string.
+        /// </summary>
+        public const string LabelOrText = "LabelOrText"; 
+
+        /// <summary>
+        /// Gets the key for the LabelSecurityText resource string.
+        /// </summary>
+        public const string LabelSecurityText = "LabelSecurityText"; 
+
+        /// <summary>
+        /// Gets the key for the LabelStartsWithText resource string.
+        /// </summary>
+        public const string LabelStartsWithText = "LabelStartsWithText"; 
+
+        /// <summary>
+        /// Gets the key for the LabelSystemText resource string.
+        /// </summary>
+        public const string LabelSystemText = "LabelSystemText"; 
+
+        /// <summary>
+        /// Gets the key for the LabelWarningText resource string.
+        /// </summary>
+        public const string LabelWarningText = "LabelWarningText"; 
+
+        /// <summary>
+        /// Gets the key for the LabelYesText resource string.
+        /// </summary>
+        public const string LabelYesText = "LabelYesText"; 
+
+        /// <summary>
+        /// Gets the key for the LockedByText resource string.
+        /// </summary>
+        public const string LockedByText = "LockedByText"; 
+
+        /// <summary>
+        /// Gets the key for the LockedForReviewByText resource string.
+        /// </summary>
+        public const string LockedForReviewByText = "LockedForReviewByText"; 
+
+        /// <summary>
+        /// Gets the key for the LockedForTranslationByText resource string.
+        /// </summary>
+        public const string LockedForTranslationByText = "LockedForTranslationByText"; 
+
+        /// <summary>
+        /// Gets the key for the RuntimeMessageText resource string.
+        /// </summary>
+        public const string RuntimeMessageText = "RuntimeMessageText"; 
     }
 }
